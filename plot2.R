848180ac87cf71b8a101c3801d0f90ce9981d1a6
PelinalWS <batucan971@gmail.com>
2025-03-21 00:45:13 +0300

    removed the time from the x axis

--- plot2.R
+++ plot2.R
@@ -1,6 +1,10 @@
 data <- read.table("./household_power_consumption.txt", header= TRUE, sep= ";", na.strings = "?")
-days <- data[((data$Date=='1/2/2007'|data$Date=='2/2/2007')),]
+days <- data[((data$Date=="1/2/2007"|data$Date=="2/2/2007")),]
 days$DateTime <- strptime(paste(days$Date, days$Time), format= "%d/%m/ %Y %H:%M:%S")
-png(filename = 'plot2.png',width = 480, height = 480)
-plot(x= days$DateTime, y= days$Global_active_power, type= 'l', xlab="", ylab= "Global Active Power (kilowatts)")
+
+png(filename = "plot2.png", width= 480, height= 480)
+
+with(days, plot(Global_active_power~DateTime, type= "l", ylab= "Global Active Power (kilowatts)", xaxt= "n"))
+with(days, axis.POSIXct(1, at= c(min(DateTime), median(DateTime), max(DateTime)), labels= c("Thu", "Fri", "Sat")))
+
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,21 +1,23 @@
 data <- read.table("./household_power_consumption.txt", header= TRUE, sep= ";", na.strings = "?")
-days <- data[((data$Date=='1/2/2007'|data$Date=='2/2/2007')),]
+days <- data[((data$Date=="1/2/2007"|data$Date=="2/2/2007")),]
 days$DateTime <- strptime(paste(days$Date, days$Time), format= "%d/%m/ %Y %H:%M:%S")
 
-png(filename = 'plot4.png',width = 480, height = 480)
+png(filename = "plot4.png",width= 480, height= 480)
 
 par(mfrow= c(2,2))
 
-with(days, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+with(days, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power", xaxt= "n"))
+with(days, axis.POSIXct(1, at= c(min(DateTime), median(DateTime), max(DateTime)), labels= c("Thu", "Fri", "Sat")))
 
-with(days, plot(DateTime, Voltage, type="l", xlab="datetime",ylab="Voltage"))
+with(days, plot(DateTime, Voltage, type="l", xlab="datetime",ylab="Voltage", xaxt= "n"))
+with(days, axis.POSIXct(1, at= c(min(DateTime), median(DateTime), max(DateTime)), labels= c("Thu", "Fri", "Sat")))
 
-with(days, plot(DateTime, Sub_metering_1, type="l", xlab= "", ylab= "Energy sub metering"))
+with(days, plot(DateTime, Sub_metering_1, type="l", xlab= "", ylab= "Energy sub metering", xaxt= "n"))
 with(days, lines(DateTime, Sub_metering_2, col= "red"))
 with(days, lines(DateTime, Sub_metering_3, col= "blue"))
+with(days, axis.POSIXct(1, at= c(min(DateTime), median(DateTime), max(DateTime)), labels= c("Thu", "Fri", "Sat")))
+legend("topright", lty= c(1,1,1), col= c("black", "red", "blue"), legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty= "n")
 
-legend("topright", lty= c(1,1,1), col= c("black", "red", "blue"), legend= c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"ab= "datetime"), bty= "n")
-
-with(days, plot(DateTime, Global_reactive_power, type= "l", xlab= "datetime", ylab= "Global_reactive_power"))
+with(days, plot(DateTime, Global_reactive_power, type= "l", xlab= "datetime", ylab= "Global_reactive_power", xaxt= "n"))
 
 dev.off()
